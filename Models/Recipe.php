bbfaf2342f92499892f8c7be92c586dc2a81c41c
Taner Ozonaran <t.ozonaran@gmail.com>
2014-09-05 05:33:59 +1000

    Define the Recipe Model

--- Models/Recipe.php
+++ Models/Recipe.php
@@ -11,6 +11,61 @@ namespace Models\Recipefinder;
  * @version    1.0.0
  * @author Taner Ozonaran <t.ozonaran@gmail.com>
  */
-class Recipe {    
 
+class Recipe {
+    
+    /**
+     * name
+     * 
+     * The name of the Recipe
+     * 
+     * @var string
+     * @access private
+     */
+    private $name;
+    
+    /**
+     * ingredients
+     * 
+     * Stores any array of Ingredients for the Recipe
+     * 
+     * @var array of Models\Recipefinder\Ingredient
+     * @access private
+     */
+    private $ingredients = array();
+    
+    /**
+     * usedBy
+     * 
+     * Stores the used by date and time of the recipe. This value will be based on
+     * the lowest used by date of the ingredients used 
+     * 
+     * @var time
+     * @access private
+     */
+    private $usedBy;
+    
+    public function __construct($name = '') {
+        $this->name = $name;
+    }
+
+    public function getIngredients() {
+        return $this->ingredients;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function addIngredient(Ingredient $ingredient) {
+        $this->ingredients[] = $ingredient;
+    }
+    
+    public function getIngredientUsedBy() {
+        return $this->usedBy;
+    }
+    
+    public function setUsedBy($usedBy) {
+        $this->usedBy = $usedBy;
+    }
 }
